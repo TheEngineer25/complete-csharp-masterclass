c17f96e18f3c21fcc5c4383c0abb4bf627b0e676
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-05 14:03:18 +0300

    vault backup: 2026-09-05 14:03:18

--- Лекции/Раздел 1. Въведение/5. Изпозлване на променливи - стрингове/Код/UsingVariablesStrings/Program.cs
+++ Лекции/Раздел 1. Въведение/5. Изпозлване на променливи - стрингове/Код/UsingVariablesStrings/Program.cs
@@ -1,3 +1,8 @@
 ﻿string myFriendsName;
 myFriendsName = "Stefan";
 System.Console.WriteLine(myFriendsName);
+
+myFriendsName = "Viktor";
+System.Console.WriteLine(myFriendsName);
+
+string myFriendsName2 = "Plamen";
