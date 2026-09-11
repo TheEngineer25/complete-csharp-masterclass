5da8098aa652fede6fa95c24b4f6322915b7ec02
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-11 16:01:24 +0300

    vault backup: 2026-09-11 16:01:24

--- Code/usingVariables-Strings/Program.cs
+++ Code/usingVariables-Strings/Program.cs
@@ -1,5 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
-
-string myFriendsName;
+﻿string myFriendsName;
 myFriendsName = "Stefan";
 System.Console.WriteLine(myFriendsName);
+
+myFriendsName = "Viktor";
+
+System.Console.WriteLine(myFriendsName);
+
+string myFriendsName2 = "Plamen";
