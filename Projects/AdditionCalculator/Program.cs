129eae7428af48fa6fef84fcade37593e48f6ef0
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-11 16:52:09 +0300

    vault backup: 2026-09-11 16:52:09

--- Projects/AdditionCalculator/Program.cs
+++ Projects/AdditionCalculator/Program.cs
@@ -1,5 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter something!");
 
 string userInput = Console.ReadLine();
 
 System.Console.WriteLine("You entered " + userInput);
+
+Console.ReadKey();
