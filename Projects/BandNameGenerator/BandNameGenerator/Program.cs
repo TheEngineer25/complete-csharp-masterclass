4852a7516466a39653d8b42ae4fc3dda6682435b
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-13 19:45:47 +0300

    Band Name Generator

---
+++ Projects/BandNameGenerator/BandNameGenerator/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What is the name of the city you were born?");
+string city = Console.ReadLine();
+Console.WriteLine("What was the name of your last pet?");
+string pet = Console.ReadLine();
+
+Console.WriteLine($"Your band's name could be {city} {pet}");
+
+Console.ReadKey();
