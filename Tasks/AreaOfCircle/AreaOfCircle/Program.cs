7ad01c322ce950e73c3a58545b33095272170cf0
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-13 19:31:06 +0300

    area of circle task

---
+++ Tasks/AreaOfCircle/AreaOfCircle/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Instructions
+The radius value can be assigned to a variable; need not to take the value from the keyboard input.
+
+Formula: PI * radius * radius
+*/
+
+double radius = 5.67;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
+
+Console.ReadKey();
