03f531ead843868a7945124a49f5b88712a49478
Владислав Иванов <vlado.ivanov1990@gmail.com>
2026-09-13 15:16:05 +0300

    vault backup: 2026-09-13 15:16:04

--- Projects/AdditionCalculator/Program.cs
+++ Projects/AdditionCalculator/Program.cs
@@ -1,15 +1,19 @@
-﻿Console.WriteLine("Enter a whole number:");
+﻿Console.WriteLine("Enter the first number number:");
 
 int myNumber = 0;
-
-
+int myNumber2 = 0;
 
 string userInput = Console.ReadLine();
 
 myNumber = int.Parse(userInput);
 
+Console.WriteLine("Enter the second number:");
 
+myNumber2 = int.Parse(userInput);
 
-System.Console.WriteLine("You entered " + userInput);
+int result = myNumber = myNumber2;
+
+Console.WriteLine("The sum of " + myNumber + " and " + myNumber2 + " is " + result);
+
 
 Console.ReadKey();
